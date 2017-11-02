e9e1358b8887c205ee227edfea293612e51720af
Charles Burgess <consulting@cvburgess.com>
2017-11-02 10:11:03 -0400

    Fix export syntax

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-exports.default = require('./lib/withData');
+"use strict";
+
+var withData = require("./lib/withData").default;
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = withData;
+module.exports = exports["default"];
