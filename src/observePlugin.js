46496648cb05ef5177c4719df95a8af0328e2236
Zach Kanzler <zkanzler@perchsecurity.com>
2018-05-16 11:56:42 -0400

    Add link to original observe plugin

--- src/observePlugin.js
+++ src/observePlugin.js
@@ -2,6 +2,7 @@ import eventsPlugin from 'store/plugins/events';
 
 
 // store.js observe plugin which doesn't immediately invoke callback upon observing
+// Original source: https://github.com/marcuswestin/store.js/blob/b8e22fea8738fc19da4d9e7dbf1cda6e5185c481/plugins/observe.js
 const observePlugin = () => ({
   observe(_, key, callback) {
     return this.watch(key, callback);
